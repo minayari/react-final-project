97373ec9457ff6466b9f6c336df2028dbd515eaa
MinaAllahyari <minaallahyari.uni@gmail.com>
2025-05-04 19:12:23 +0330

    dynamic search (not complete)

--- src/pages/MianPage.jsx
+++ src/pages/MianPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import api from "../api/api";
 import SingleProduct from "../components/SingleProduct";
 import { useQuery } from "@tanstack/react-query";
@@ -8,6 +8,14 @@ import Modal from "../components/Modal";
 
 export default function MainPage() {
   const { data: products, isLoading, isError, error } = useGetProducts();
+  const [searchProduct, setSearchProducts] = useState("");
+
+  //Desn't work in UseMemo + price checking has issues
+  const filteredProducts = products?.data?.filter(
+    (item) =>
+      item.title.toLowerCase().includes(searchProduct.toLowerCase()) ||
+      item.price === searchProduct.toString()
+  );
 
   if (isError) {
     return <Modal message={error.message} />;
@@ -21,20 +29,32 @@ export default function MainPage() {
     <>
       <div className="flex justify-center items-center mb-[2rem]">
         <input
+          value={searchProduct}
+          onChange={(evt) => setSearchProducts(evt.target.value)}
           type="text"
           className="w-[20rem] border-[2px] border-solid border-cyan-800 rounded-[0.5rem] focus: outline-none p-[0.15rem]"
         />
       </div>
       <div className="grid grid-cols-4 gap-4">
-        {products?.data?.map((item) => (
-          <SingleProduct
-            key={item.id}
-            id={item.id}
-            image={item.image}
-            title={item.title}
-            price={item.price}
-          />
-        ))}
+        {filteredProducts
+          ? filteredProducts.map((item) => (
+              <SingleProduct
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                title={item.title}
+                price={item.price}
+              />
+            ))
+          : products?.data?.map((item) => (
+              <SingleProduct
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                title={item.title}
+                price={item.price}
+              />
+            ))}
       </div>
     </>
   );
