abb39a8f6c06a8b7577168012808454c31943edd
MinaAllahyari <minaallahyari.uni@gmail.com>
2025-05-06 16:07:53 +0330

    until categories

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,8 +2,10 @@ import { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Category({ products }) {
-  //   const [categorized, setCategorized] = useState([]);
   const navigateToCategoryPage = useNavigate();
+
+  const STYLE =
+    "text-center m-[0.5rem] border-[2px] border-solid border-cyan-800 rounded-[1rem] transition duration-300 ease-in-out transform scale-100 hover:scale-105 transition duration-300 hover:bg-cyan-800 hover:text-white hover:cursor-pointer ";
 
   const handelCategory = useCallback(
     (category) => {
@@ -14,30 +16,30 @@ export default function Category({ products }) {
   );
 
   return (
-    <div className="grid grid-cols-2 gap-10 h-[15rem]">
+    <div className="grid grid-cols-2 gap-10 h-[15rem] mb-[2rem] ">
       <div
-        className=" text-center m-[0.5rem] border-[2px] border-solid border-cyan-800 rounded-[1rem] hover:bg-cyan-800 hover:text-white hover:cursor-pointer "
+        className={STYLE}
         data-category="men's clothing"
         onClick={(evt) => handelCategory(evt.currentTarget.dataset.category)}
       >
         Men's Clothes
       </div>
       <div
-        className=" text-center m-[0.5rem] border-[2px] border-solid border-cyan-800 rounded-[1rem] hover:bg-cyan-800 hover:text-white hover:cursor-pointer "
+        className={STYLE}
         data-category="women's clothing"
         onClick={(evt) => handelCategory(evt.currentTarget.dataset.category)}
       >
         Women's Clothes
       </div>
       <div
-        className=" text-center m-[0.5rem] border-[2px] border-solid border-cyan-800 rounded-[1rem] hover:bg-cyan-800 hover:text-white hover:cursor-pointer "
+        className={STYLE}
         data-category="jewelery"
         onClick={(evt) => handelCategory(evt.currentTarget.dataset.category)}
       >
         Accessories
       </div>
       <div
-        className=" text-center m-[0.5rem] border-[2px] border-solid border-cyan-800 rounded-[1rem] hover:bg-cyan-800 hover:text-white hover:cursor-pointer "
+        className={STYLE}
         data-category="electronics"
         onClick={(evt) => handelCategory(evt.currentTarget.dataset.category)}
       >
